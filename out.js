c1a355080e5c76d2a07cfc3d9b03bfc143f6dd75
Bhanu <bhanuchander.m@iprismtech.com>
2023-02-21 18:11:00 +0530

    sample output added

---
+++ out.js
@@ -0,0 +1,20 @@
+{
+    "status": true,
+    "message": "Product details fetched successfully",
+    "response": [
+        {
+            "product_id": "2",
+            "name": "INTERIOR CLEANING",
+            "image": "assets/images/product_images/1674236493.jpg",
+            "time": "30 Min",
+            "price": "1500",
+            "description": "dsfvkdsjhc sduhdsn c sudsojcsod ncosdncs  sduicsd cusdcn sduc n udcnnsd dshcsdjc ",
+            "delete_status": "1",
+            "created_on": "2022-12-06",
+            "modified_on": "2023-01-20",
+            "total": "1500",
+            "cp_total": "1500",
+            "add_on_products": []
+        }
+    ]
+}
